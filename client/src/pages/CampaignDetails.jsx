66fb8c1510b00404fa5418a20daf66ad64117395
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-03-26 23:10:37 +0530

    update responsiveness for mobile devices

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -87,7 +87,7 @@ const CampaignDetails = () => {
 
               <div className="mt-[20px] flex flex-col gap-4">
                 {donators.length > 0 ? donators.map((item, index) => (
-                  <div key={`${item.donator}-${index}`} className="flex justify-between items-center gap-4">
+                  <div key={`${item.donator}-${index}`} className="flex overflow-auto hover:overflow-scroll justify-between items-center gap-4">
                     <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {item.donator}</p>
                     <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.donation}</p>
                   </div>
